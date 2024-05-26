b94df0b66d8c0e245ca66610f42aecc7ee59ed9e
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2024-05-26 20:30:17 +0200

    remove obsolete comment

--- cuda_rasterizer/backward.cu
+++ cuda_rasterizer/backward.cu
@@ -405,7 +405,7 @@ renderCUDA(
 				continue;
 
 			const float G = exp(power);
-            const float alpha = min(0.99f, con_o.w * G); // todo: fix gradient here
+            const float alpha = min(0.99f, con_o.w * G);
             if (alpha < 1.0f / 255.0f)
                 continue;
 
